8f0e4418d5b86939f49d8013fcb3a1d5571e0395
Cesar Ticona <cesarju.cc@gmail.com>
2023-09-15 23:21:33 -0400

    clase de javascript intro

---
+++ string.js
@@ -0,0 +1,8 @@
+let string = "Hola clase de Ada Shool";
+// 0 1 2 3 4 5 6 ...
+
+console.log(string.length);
+console.log(string[2]);
+console.log(string[3]);
+console.log(string[7]);
+console.log(string.indexOf("de"));
---
+++ variable.js
@@ -0,0 +1,45 @@
+let nombre = "Cesar Ticona"; //string
+let nota = 49; //Number
+let estadoInscrito = false; // booleanos
+nota = 1;
+nombre = "Ada School";
+
+const nota2 = 100;
+//nota2 = 10; valor que no se puede cambiar
+
+//console.log(nombre);
+//console.log(nota);
+//console.log(estadoInscrito);
+//console.log(nota2);
+
+// Ejercicios con operadores aritmetico
+//let numero2 = 10;
+// 2^4 = 2 * 2 * 2 * 2
+// 39^10  = 39*39*39*39
+//let resultado = numero1 + numero2
+//console.log(resultado);
+
+let numero1 = 2;
+//numero1 = numero1 + 10;
+// console.log(numero1); //2
+numero1 += 10; //12
+// console.log(numero1);
+let resultado2 = numero1 + 10; //12 + 10
+
+//Operadores de comparacion
+// = asignacion
+// == conparacion
+// === comparacion + tipo de dato
+
+let valor1 = 3;
+let valor2 = "3";
+let resultado = valor1 === valor2;
+
+// console.log(resultado);
+
+let miEdad;
+
+miEdad = 1234;
+miEdad = "hola";
+
+// console.log(12 + 100);
